f318942969f7ffbfb96dd909b362c98ceb26bde4
Tetiana Khotiaintseva <tetiana.khotiaintseva@ardigen.com>
2016-12-08 17:03:54 +0100

    Wrap bash vars in curly braces

--- recipes/ericscript/build.sh
+++ recipes/ericscript/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-bindir=$PREFIX/bin
+outdir=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
+bindir=${PREFIX}/bin
 
-mkdir -p $outdir
-mkdir -p $bindir
-cp -r * $outdir/
-chmod +x $outdir/ericscript.pl
-ln -s $outdir/ericscript.pl $bindir
+mkdir -p ${outdir}
+mkdir -p ${bindir}
+cp -r * ${outdir}/
+chmod +x ${outdir}/ericscript.pl
+ln -s ${outdir}/ericscript.pl ${bindir}
